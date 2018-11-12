e9036481dc48f7e4bc2bb4cf81fb1403905cfe1a
Laura Young <laura@Lauras-MacBook-Pro.local>
2018-11-12 11:42:32 +0000

    app updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,37 +13,42 @@ import org.apache.lucene.store.Directory;
 
 
 public class App {
-    public static void main( String[] args ) throws IOException, ParseException, QueryNodeException {
-    	Analyzer analyzer = CustomAnalyzer.builder()
-                .withTokenizer("standard")
-                .addTokenFilter("lowercase")
-                .addTokenFilter("stop")
-                .addTokenFilter("porterstem")
-                .build();
-        MyIndexWriter iw = MyIndexWriter.getInstance();
+	public static void main( String[] args ) throws IOException, ParseException, QueryNodeException {
+		Analyzer analyzer = CustomAnalyzer.builder()
+				.withTokenizer("standard")
+				.addTokenFilter("lowercase")
+				.addTokenFilter("stop")
+				.addTokenFilter("porterstem")
+				.build();
+		MyIndexWriter iw = MyIndexWriter.getInstance();
 
-        Directory index = iw.index(1, analyzer);
+		//Directory index = iw.index(0, analyzer);
 
-        //testing query parsing
-        MyIndexSearcher searcher = MyIndexSearcher.getInstance();
-        searcher.parseQuery();
-             //Directory index = iw.index(i, analyzer);
-             String fileName = "trec_res_" + iw.getConfig().getSimilarity().toString();
-             fileName = fileName.replaceAll("\\p{P}","");
-            BufferedWriter writer = new BufferedWriter(new FileWriter("../lucene_assignment2/results/" + fileName));
-            searcher.search(iw.getConfig(), index, writer, analyzer);
-            writer.close();
+		//testing query parsing
+		//        MyIndexSearcher searcher = MyIndexSearcher.getInstance();
+		//        searcher.parseQuery();
+		//             //Directory index = iw.index(i, analyzer);
+		//             String fileName = "trec_res_" + iw.getConfig().getSimilarity().toString();
+		//             fileName = fileName.replaceAll("\\p{P}","");
+		//            BufferedWriter writer = new BufferedWriter(new FileWriter("../lucene_assignment2/results/" + fileName));
+		//            searcher.search(iw.getConfig(), index, writer, analyzer);
+		//            writer.close();
 
-//
-//        must update
-//
-        // for (int i = 0; i < 2; i++) {
-        //     Directory index = iw.index(i, analyzer);
-        //     String fileName = "trec_res_" + iw.getConfig().getSimilarity().toString();
-        //     fileName = fileName.replaceAll("\\p{P}","");
-        //     BufferedWriter writer = new BufferedWriter(new FileWriter("../lucene_assignment/results/" + fileName));
-        //     searcher.search(iw.getConfig(), index, writer, analyzer);
-        //     writer.close();
-        // }
-    }
+		//
+		//        must update
+		//
+		for (int i = 0; i < 2; i++) {
+			//Directory index = iw.index(0, analyzer);
+
+			//testing query parsing
+			MyIndexSearcher searcher = MyIndexSearcher.getInstance();
+			searcher.parseQuery();
+			Directory index = iw.index(i, analyzer);
+			String fileName = "trec_res_" + iw.getConfig().getSimilarity().toString();
+			fileName = fileName.replaceAll("\\p{P}","");
+			BufferedWriter writer = new BufferedWriter(new FileWriter("../lucene_assignment2/results/" + fileName));
+			searcher.search(iw.getConfig(), index, writer, analyzer);
+			writer.close();
+		}
+	}
 }
